672470f347bcaeb74d0c274156d566b3f246b69d
everafterinvites <contact.everafterinvites@gmail.com>
2026-07-30 17:12:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/envelope-template/',
+    base: '/Eslam-x-Halla/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
